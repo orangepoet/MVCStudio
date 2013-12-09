8f5e462bd4af06ef9f9f7bdca4fbfa4bee280d54
orangepoet <chengzhi37@gmail.com>
2013-12-09 14:27:50 +0800

    pdf demos

---
+++ Mvc.Web/Controllers/PdfController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc.Web.Core.Pdf;
+
+namespace Mvc.Web.Controllers {
+    public class PdfController : Controller {
+        public ViewResult Pdf() {
+            return new PdfResult();
+        }
+
+        public ViewResult Pdf(object model) {
+            return new PdfResult(model);
+        }
+
+        public ViewResult Pdf(object model, string viewName) {
+            return new PdfResult(model, viewName);
+        }
+
+        public ViewResult Pdf(object model, string viewName, string pdfDownloadName) {
+            return new PdfResult(model, viewName, pdfDownloadName);
+        }
+    }
+
+    public static class ControllerExt {
+        public static ViewResult Pdf(this Controller controller) {
+            return new PdfResult();
+        }
+
+        public static ViewResult Pdf(this Controller controller
+            , object model) {
+            return new PdfResult(model);
+        }
+
+        public static ViewResult Pdf(this Controller controller
+            , object model, string viewName) {
+            return new PdfResult(model, viewName);
+        }
+
+        public static ViewResult Pdf(this Controller controller
+            , object model, string viewName, string pdfDownloadName) {
+            return new PdfResult(model, viewName, pdfDownloadName);
+        }
+    }
+}
---
+++ Mvc.Web/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc.Web.Models {
+    public class Person {
+        public string UserName { get; set; }
+        public int LuckyNumber { get; set; }
+    }
+}
