7be00ecddeba10c75f1918c824ba186e9cb92e0e
orangepoet <chengzhi37@gmail.com>
2021-07-15 23:42:43 +0800

    mod custom cs

--- Mvc.BLL/ICustomerMgr.cs
+++ Mvc.BLL/ICustomerMgr.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 namespace Mvc.BLL {
     public interface ICustomerMgr {
+        // comments
         void AddCustomer(Mvc.Models.Entities.Customer c);
         Mvc.Models.Entities.Customer GetCustomer(int id);
         System.Linq.IQueryable<Mvc.Models.Entities.Customer> GetCustomers();
